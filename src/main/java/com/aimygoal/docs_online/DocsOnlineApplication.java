293ab36b1b19ba1b3d5e353a6a66ba9ff5e6b11a
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-10-24 10:49:23 +0200

    creating the git project

---
+++ src/main/java/com/aimygoal/docs_online/DocsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.aimygoal.docs_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimygoal/docs_online/DocsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimygoal.docs_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
